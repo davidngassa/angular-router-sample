22477fa907034cffaca979fa14aa3c392cded7e3
David Ngassa <ngassadavid97@gmail.com>
2021-01-11 13:14:01 +0400

    :white_check_mark: Successfully copied all heroes files from previous project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
-import { HeroesListComponent } from './heroes-list/heroes-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HeroesModule } from './heroes/heroes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CrisisListComponent,
-    HeroesListComponent,
     PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HeroesModule,
+    AppRoutingModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
