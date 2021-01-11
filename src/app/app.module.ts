4b1adf142a1bb511b03dc1349460f75d666cd22b
David Ngassa <ngassadavid97@gmail.com>
2021-01-11 10:58:22 +0400

    :truck: defining routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
@@ -12,7 +13,11 @@ import { HeroesListComponent } from './heroes-list/heroes-list.component';
     HeroesListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'crisis-list', component:CrisisListComponent},
+      {path: 'heroes-list', component:HeroesListComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
