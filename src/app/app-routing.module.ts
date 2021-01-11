a87a216a5e33f7477a21de5e37dee89d5b3f4d5e
David Ngassa <ngassadavid97@gmail.com>
2021-01-11 12:56:57 +0400

    :bug: Fix major routing bug

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router'
 
 import {CrisisListComponent} from './crisis-list/crisis-list.component'
@@ -7,20 +6,20 @@ import {HeroesListComponent} from './heroes-list/heroes-list.component'
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component'
 
 const appRoutes: Routes =[
-  {path: 'crisis-list', component:CrisisListComponent},
-  {path: 'heroes-list', component:HeroesListComponent},
+  {path: 'crisis-center', component:CrisisListComponent},
+  {path: 'heroes', component:HeroesListComponent},
   {path:'', redirectTo:'/heroes-list', pathMatch:'full'},
   {path:'**', component:PageNotFoundComponent}
 ]
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule,
+  imports: [    
     RouterModule.forRoot(
       appRoutes, 
       {enableTracing: true} //For debugging purposes only
       ),
-  ]
+  ],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
